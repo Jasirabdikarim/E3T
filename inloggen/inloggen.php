b6d065113b6128184f703d16c2159f9fa069432a
JesperSchuurman <jesper.schuurman@student.nhlstenden.com>
2022-12-14 11:58:19 +0100

    Made most of index.html

---
+++ inloggen/inloggen.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" type="text/css" href="../css/style.css">
+            <title>E3T</title>
+        </head>
+
+        <body>
+            <header>
+                <div id="headerContent">
+                    <h2>E3T</h2>
+                </div>
+                <nav>
+                    <a href="../talenten/talenten.php">Talenten</a>
+                    <a href="../evenementen/evenementen.php">Evenementen</a>
+                    <a href="inloggen.php">Inloggen</a>
+                </nav>
+            </header>
+        </body>
+    </html>
