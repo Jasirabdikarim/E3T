316f78258b98b934bc789365b01c32fd5d2a63c8
Thomas Oole <thomas.oole@student.nhlstenden.com>
2022-12-19 15:17:54 +0100

    Talents page profile layout

--- talenten.php
+++ talenten.php
@@ -18,5 +18,19 @@
                     <a href="inloggen.php">Inloggen</a>
                 </nav>
             </header>
+			<main>
+				<div class ="purplebar"></div>
+					<div class ="talentprofile">
+						<img src="img/.." alt="img" class="talentsprofileimg">
+							<div class ="talentprofiletext">
+								<h2> Voornaam Achternaam </h2>
+								<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							</div>
+					</div>
+				<div class ="purplebar"></div>
+			</main>
+			<footer>
+			
+			</footer>
         </body>
     </html>
