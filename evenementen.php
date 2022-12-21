884437d01f739100c760d1d9bc6bf71efa5842f4
Martijn van der Veen <martijn.van.der.veen@student.nhlstenden.com>
2022-12-21 13:46:40 +0100

    evenementen.php html and css site done

--- evenementen.php
+++ evenementen.php
@@ -20,13 +20,58 @@
                     <a href="inloggen.php">Inloggen</a>
                 </nav>
             </header>
-            <div id="Evenementen";> 
-                <h1> Evenementen </h2> 
-            </div> 
-            <div class ="purplebar"></div>
-
-            <div id="subfooter";>
-                <p>Privacy Policy  l  Algemene voorwaarden  l  Disclaimer  l  Cookies</p>
-             </div>
+            <main id="background"> 
+                <div id="Evenementen";> 
+                    <h1> Evenementen </h2> 
+                </div> 
+                <div class ="purplebarwidth"></div>
+                <div id ="aankomende">
+                    <h1> Aankomende Evenementen <h1> 
+                </div> 
+                <div id="searchform">
+                    <form method="GET" action="talenten.php">
+                        <input type="text" id="search"  placeholder="search...">
+                    </form>
+                </div>
+                 <div id="agenda2">
+                    <table>
+                        <tr>
+                            <th>Evenementen</th>
+                            <th>Datum</th> 
+                            <th>Talenten</th> 
+                            <th>Locatie</th> 
+                            <th>Prijs</th> 
+                            <th>Overig</th> 
+                        </tr>
+                        <tr>
+                            <<th>Evenementen</th>
+                            <th>Datum</th> 
+                            <th>Talenten</th> 
+                            <th>Locatie</th> 
+                            <th>Prijs</th> 
+                            <th>Overig</th>
+                        </tr>
+                        <tr>
+                        <th>Evenementen</th>
+                            <th>Datum</th> 
+                            <th>Talenten</th> 
+                            <th>Locatie</th> 
+                            <th>Prijs</th> 
+                            <th>Overig</th>
+                        </tr>
+                        <tr>
+                            <th>Evenementen</th>
+                            <th>Datum</th> 
+                            <th>Talenten</th> 
+                            <th>Locatie</th> 
+                            <th>Prijs</th> 
+                            <th>Overig</th>
+                        </tr>
+                        </table>
+                    </div>
+                <div id="subfooter";>
+                    <p>Privacy Policy  l  Algemene voorwaarden  l  Disclaimer  l  Cookies</p>
+                </div>
+            </main>
         </body>
-    </html>
+    </html> 
